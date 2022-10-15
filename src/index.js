07e9c60fc20d618b5876e92654ccad464e5c9290
ShaoyaHUANG <shaoyahuang22@gmail.com>
2022-10-15 15:30:49 +0800

    change back to browserRouter

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Game from './routes/Game';
 import Pokemon from './routes/Pokemon';
 import NotFound from './routes/NotFound';
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter basename="Pokedex">
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="Game" element={<Game />} />
       <Route path="pokemon/:id" element={<Pokemon />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
