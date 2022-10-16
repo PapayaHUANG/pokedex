8a04cb15f551e94e165d5c5981ac715de7675cd6
ShaoyaHUANG <shaoyahuang22@gmail.com>
2022-10-16 12:26:31 +0800

    use pagehook in search bar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback } from 'react';
 import { FaSearch } from 'react-icons/fa';
-
+import { usePageNum } from '../helper/PageNumHooks';
 import Debounce from '../helper/useDebounce';
 import '../styles/Searchbar.css';
 
 export default function Searchbar({ setSearchWord }) {
   const [value, setValue] = useState('');
-
+  const { turnToDefaultPage } = usePageNum();
   const debouncedSave = useCallback(
     Debounce((nextValue) => setSearchWord(nextValue), 3000),
     []
   );
 
+  const debouncedTurnToDefaultPage = useCallback(
+    Debounce(() => turnToDefaultPage(), 3000)
+  );
   const handleChange = (e) => {
     const { value: nextValue } = e.target;
     setValue(nextValue);
     debouncedSave(nextValue);
+    debouncedTurnToDefaultPage();
   };
 
+  console.log(value);
   return (
     <div className="searchbar">
       <form>
